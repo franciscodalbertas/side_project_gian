f4cef9aa44d1d10be89b604c3aff87f70e7efb60
gcerullo <gcerullo95@gmail.com>
2024-11-21 17:35:49 -0800

    update LIFE layer

--- scripts/04_reproject_life.R
+++ scripts/04_reproject_life.R
@@ -9,12 +9,13 @@ library(stringr)
 base_raster <- rast("rasters/world_base_moll_1km.tif")
 
 #Read in LIFE (not sure if this is the latest version)
-LIFE_path <- "rasters/Eyres_et_al_LIFE-2024/Eyres_et_al_LIFE/"
-LIFE <- rast(paste0(LIFE_path, "restore_0.25.tif"))
+#LIFE_path <- "rasters/Eyres_et_al_LIFE-2024/Eyres_et_al_LIFE/" #old version 
+LIFE_path <- "rasters/deltap_final/deltap_final/" #old version 
+
+LIFE <- rast(paste0(LIFE_path, "summed_scaled_restore_0.25.tif"))
 
 #sum value for amphibians, birds, mammals and reptiles to get LIFE score 
 #across taxa 
-LIFE$score <- app(LIFE, sum)
 plot(LIFE)
 
 # Reproject LIFE to match the resolution and CRS of base raster
